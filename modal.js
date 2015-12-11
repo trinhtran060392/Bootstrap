ddba782ddd0d1061966935d67bee2961da2664de
TrinhTV3 <trinhtv060392@gmail.com>
2015-12-11 13:34:03 +0700

    foo

--- modal.js
+++ modal.js
@@ -7,5 +7,18 @@ $(document).ready(function() {
 			modal.find('.modal-title').text('New message '+ recip);
 			modal.find('.modal-body input').val(recip);
 		})
+		$('#modalData').on('hide.bs.modal',function(event){
+			alert("you are closing model");
+		})
+		$('#modalData').on('hidden.bs.modal',function(event){
+			alert('you closed your modal');
+		})
 
+
+		$('button[data-target="#myModal"]').on('click',function(){
+			$('#myModal').modal({
+				keyboard: false,
+				backdrop: false
+			});
+		});
 	});
